0688fbe3d3d1a7ff3fededddc5fc56dcdad8ac23
Love-Becca <follycube2020@gmail.com>
2024-12-23 09:46:27 +0100

    change addFood logic

--- src/repositories/WestAfricaFoodRepository.js
+++ src/repositories/WestAfricaFoodRepository.js
@@ -6,25 +6,32 @@ class WestAfricaFoodRepository {
     }
 
     async addFoodData(fileData) {
-        const foodList = Object.values(fileData).map(data => ({
-            foodCode: data["FOODCODE"],
-            foodName: data["FOOD NAMEIN ENGLISH"] || "Unknown Food Name",
-            nutrients: data,
-        }));
-
+        const foodList = Object.values(fileData)
+            .filter(data => data["FOODCODE"] && data["FOOD NAMEIN ENGLISH"]) // Filter valid entries
+            .map(data => ({
+                foodCode: data["FOODCODE"],
+                foodName: data["FOOD NAMEIN ENGLISH"] || "Unknown Food Name",
+                nutrients: data,
+            }));
+    
         try {
+            if (foodList.length === 0) {
+                console.log("No valid food data found to add.");
+                return;
+            }
+    
             const resp = await this.Model.collection.bulkWrite(
                 foodList.map(food => ({
                     insertOne: { document: food }
                 }))
             );
-            console.log("Data successfully added");
+            console.log("Data successfully added:", resp.insertedCount);
             return resp;
         } catch (error) {
             console.error("Error adding food data:", error.message);
         }
     }
-
+    
     async getAllFood() {
         return await this.Model.find();
     }
