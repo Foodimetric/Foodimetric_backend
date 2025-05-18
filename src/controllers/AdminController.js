bf53624b10c3ef190bbaa57fee034582295976b1
Love-Becca <follycube2020@gmail.com>
2025-05-18 16:13:39 +0100

    Fix error

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -182,9 +182,11 @@ class AdminController {
 
             const { weeklyCalculations, monthlyCalculations, yearlyCalculations } = getAnalyticsBreakdown(dailyCalculations);
             const { weeklyCalculations: weeklyFoodLogs, monthlyCalculations: monthlyFoodLogs, yearlyCalculations: yearlyFoodLogs } = getAnalyticsBreakdown(rawDailyFoodDiaryLogs);
-            const newsletterSubscribers = Newsletter.find().sort({ createdAt: -1 }).select("email createdAt")
-
-
+            // const newsletterSubscribers = Newsletter.find().sort({ createdAt: -1 }).select("email createdAt")
+            const newsletterSubscribers = Newsletter.find()
+                .sort({ createdAt: -1 })
+                .select("email createdAt")
+                .lean()
             return res.json({
                 totalUsers,
                 totalAnthropometricCalculations,
