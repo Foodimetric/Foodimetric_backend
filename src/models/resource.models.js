6080b56b7606a34890b1d96680aa4eb7b626686a
Love-Becca <follycube2020@gmail.com>
2025-12-05 15:31:03 +0100

    shit fixed

--- src/models/resource.models.js
+++ src/models/resource.models.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const ResourceSchema = new mongoose.Schema(
-    {
-        title: { type: String, required: true },
-        image: { type: String, required: true },
-        category: {
-            type: String,
-            enum: ["AI", "RECIPES", "EVENTS", "VIDEOS", "ARTICLES"],
-            required: true
-        },
-        date: { type: String, required: true },
-        description: { type: String },
-        author: { type: String },
-        likes: { type: Number, default: 0 },
-        summary: { type: String },
-        content: { type: String },
+const ResourceSchema = new Schema({
+    title: { type: String, required: true },
+    image: { type: String, required: true },
+    category: {
+        type: String,
+        enum: ["AI", "RECIPES", "EVENTS", "VIDEOS", "ARTICLES"],
+        required: true
     },
+    date: { type: String, required: true },
+    description: { type: String },
+    author: { type: String },
+    likes: { type: Number, default: 0 },
+    summary: { type: String },
+    content: { type: String },
+},
     { timestamps: true }
 );
-module.exports = mongoose.model("Resource", ResourceSchema);
-
+const Resource = mongoose.model("Resource", ResourceSchema);
+module.exports = Resource;
