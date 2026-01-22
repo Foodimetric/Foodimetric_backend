eb03c90497fbbf2bc34d4450411523a30a17607b
Love-Becca <follycube2020@gmail.com>
2026-01-22 16:24:06 +0100

    require login

--- src/routes/payment-routes.js
+++ src/routes/payment-routes.js
@@ -1,13 +1,15 @@
 const { PaymentController } = require("../controllers/PaymentController");
+const requireLogin = require("../utils/requireLogin")
+
 
 const route = require("express").Router();
 
-const paymentController =new PaymentController()
-route.post('/paystack/prepare-payment', paymentController.prepare)
+const paymentController = new PaymentController()
+route.post('/paystack/prepare-payment', requireLogin, paymentController.prepare)
 route.post('/paystack/initialize', paymentController.initialize)
 
 route.post('/paystack/webhook', paymentController.verify);
 route.post('/verify-promo', paymentController.verifyPromo);
-  
+
 
 module.exports = route;
